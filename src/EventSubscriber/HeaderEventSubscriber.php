fa3f7f1fffba908951ad294722f321ab3e20661d
Renée Stephen <reneestephen@gmail.com>
2022-03-02 19:27:28 -0800

    Refactor first pass.

---
+++ src/EventSubscriber/HeaderEventSubscriber.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\pantheon_drupal_edge_integrations\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Pantheon\EI\HeaderData;
+
+/**
+ * Main HeaderEventSubscriber class.
+ */
+class HeaderEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::RESPONSE][] = ['onRespond', -200];
+    return $events;
+  }
+
+  /**
+   * This method is called when the kernel.response is dispatched.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\ResponseEvent $event
+   *   The dispatched event.
+   */
+  public function onRespond(ResponseEvent $event) {
+    $config = \Drupal::configFactory()->get('pantheon_drupal_edge_integrations.config');
+
+    $response = $event->getResponse();
+
+    // Check if Vary Header should be set.
+    if (($config->get('set_vary') ?? TRUE) && method_exists($response, 'getCacheableMetadata')) {
+      // Get cache tags.
+      $tags = $response->getCacheableMetadata()->getCacheTags();
+
+      // Get existing vary headers if they've been set in other
+      // EventSubscribers.
+      $headers = $response->headers->all();
+      $vary_headers = array_key_exists('vary', $headers) ? $headers['vary'] : [];
+
+      // Add Geo to Vary header if there is a Geo decision on the page.
+      if (in_array('pantheon_drupal_edge_integrations.geo', $tags)) {
+        $vary_headers[] = 'Audience';
+      }
+      // Add Interest to Vary header if there is a Interest decision on the
+      // page.
+      if (in_array('pantheon_drupal_edge_integrations.interest', $tags)) {
+        $vary_headers[] = 'Interest';
+      }
+
+      // Retrieve and set vary header.
+      $pantheon_edge_integrations = new HeaderData();
+      $response_vary_header = $pantheon_edge_integrations->returnVaryHeader($vary_headers);
+      $response->headers->add($response_vary_header);
+    }
+  }
+
+}
